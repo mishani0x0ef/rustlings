169eb4276237fdcea5d2c378585922923bc484fa
Mykhailo.Romaniuk <mykhailo.romaniuk@eleks.com>
2024-05-27 17:47:29 +0300

    Finish module 12

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,23 +20,29 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
     Append(usize),
 }
 
+impl Command {
+    fn execute(&self, input: &String) -> String {
+        match self {
+            Command::Uppercase => input.to_uppercase(),
+            Command::Trim => input.trim().to_string(),
+            Command::Append(count) => format!("{input}{}", "bar".repeat(*count)),
+        }
+    }
+}
+
 mod my_module {
     use super::Command;
 
-    // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            output.push(command.execute(string));
         }
         output
     }
@@ -44,8 +50,7 @@ mod my_module {
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
